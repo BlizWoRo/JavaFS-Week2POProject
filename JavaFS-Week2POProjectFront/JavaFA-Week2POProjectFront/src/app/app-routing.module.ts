369e63a17a81849718e579d8d924a1b9b3325a0d
BlizWoRo <saumik148@revature.net>
2022-10-11 12:27:52 -0500

    Added directions for pages

--- JavaFS-Week2POProjectFront/JavaFA-Week2POProjectFront/src/app/app-routing.module.ts
+++ JavaFS-Week2POProjectFront/JavaFA-Week2POProjectFront/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { Page1Component } from './Pages/page1/page1.component';
+import { Page2Component } from './Pages/page2/page2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'home', component: HomeComponent },
+{path: 'page1', component: Page1Component },
+{path: 'page2', component: Page2Component },
+{path: '', redirectTo: '/home', pathMatch: 'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- JavaFS-Week2POProjectFront/JavaFA-Week2POProjectFront/src/app/app.module.ts
+++ JavaFS-Week2POProjectFront/JavaFA-Week2POProjectFront/src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { Page1Component } from './Pages/page1/page1.component';
+import { Page2Component } from './Pages/page2/page2.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Page1Component,
+    Page2Component
   ],
   imports: [
     BrowserModule,
